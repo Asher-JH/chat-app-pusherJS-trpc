1310a12f9d5d6f0990a53de32b64ebb273d99f3b
asher <asherchan2002@hotmail.com>
2022-10-24 17:51:57 +0800

    setup prisma for dev mode

--- apps/fastify-server/src/prisma.ts
+++ apps/fastify-server/src/prisma.ts
@@ -1,3 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+// add prisma to the NodeJS global type
+export let prisma: PrismaClient;
+
+declare global {
+  var __db: PrismaClient | undefined;
+}
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+  prisma.$connect();
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient();
+    global.__db.$connect();
+  }
+  prisma = global.__db;
+}
